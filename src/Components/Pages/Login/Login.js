27cfd7b02b3207da906e1434095d303125159772
Shivkumar54 <mshivkumar59@gmail.com>
2022-08-31 11:33:52 +0530

    Correction

--- src/Components/Pages/Login/Login.js
+++ src/Components/Pages/Login/Login.js
@@ -1,7 +1,10 @@
 import React from "react"
-
 const Login = () => {
-  return <div>Login</div>
+  return (
+    <div className="loginer d-flex gap-4">
+      Login Page 
+    </div>
+  )
 }
 
 export default Login
